ab41b8ae008048e3172d038c5995c3a21c244f27
zenithian <justinr345@comcast.net>
2015-01-18 16:15:19 -0700

    Worked on stuff

---
+++ Hello1/Hello1/Program.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace DeclaringConstraints
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (area = 21) {
+				double a;
+				double b;
+				double area;
+
+				a = 13;
+				b = 8;
+				area = a + b;
+
+
+
+				Console.WriteLine ("Area is Correct");
+				Console.ReadKey ();
+			}else {
+				Console.WriteLine ("Area is Not Correct");
+				Console.ReadKey ();
+			}
+
+		}
+	}
+}
